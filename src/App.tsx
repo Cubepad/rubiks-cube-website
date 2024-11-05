7afca317628cf0f389fd2e4750add13327efe12b
David Olaniyan <davideniola108@gmail.com>
2024-11-05 18:45:12 +0100

    Removed inter font and restored default mantine fonts

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,14 @@ import "@mantine/charts/styles.css";
 import "@mantine/carousel/styles.css";
 import "@mantine/nprogress/styles.css";
 import "@mantinex/mantine-logo/styles.css";
-import "./font.css";
 
-import { theme } from "./Theme";
 import { HeaderSimple } from "./HeaderSimple";
 import Hero from "./Hero";
 import { MantineProvider, AppShell, Container } from "@mantine/core";
 
 export default function App() {
   return (
-    <MantineProvider theme={theme}>
+    <MantineProvider>
       <AppShell padding="md">
         {/* Place HeaderSimple outside of AppShell.Header */}
         <Container style={{ paddingBottom: "3.75rem" }}>
--- src/Theme.ts
+++
@@ -1,5 +0,0 @@
-import { createTheme } from '@mantine/core';
-
-export const theme = createTheme({
-  fontFamily: 'Inter, -apple-system, Roboto, Segoe UI, Arial, sans-serif',
-});
