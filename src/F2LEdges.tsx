a3fc10488f7bf22df7853d6d3594ac391b43d563
David Olaniyan <davideniola108@gmail.com>
2025-01-05 20:19:37 +0100

    Add twisty player and steps if no top layer edges without yellow. Reduce container size to lg. Add 90% maxWidth for steps

--- src/F2LEdges.tsx
+++ src/F2LEdges.tsx
@@ -1,19 +1,8 @@
-import { useEffect, useRef, useState } from "react";
-import {
-  Box,
-  Title,
-  Text,
-  Card,
-  Container,
-  Button,
-  List,
-  Flex,
-  Group,
-  Pagination,
-} from "@mantine/core";
-import { useNavigate } from "react-router-dom";
-import { TwistyPlayer } from "cubing/twisty";
-import { useMediaQuery } from "@mantine/hooks";
+import {useEffect, useRef, useState} from "react";
+import {Box, Button, Card, Container, Flex, Group, List, Pagination, Text, Title,} from "@mantine/core";
+import {useNavigate} from "react-router-dom";
+import {TwistyPlayer} from "cubing/twisty";
+import {useMediaQuery} from "@mantine/hooks";
 import classes from "./CrossStep.module.css";
 
 interface EdgeStep {
@@ -26,6 +15,8 @@ const edgeScrambleRight =
   "F2 U B2 D2 U' F2 D2 B2 R2 D B2 U' L D L2 F2 R D' U B z2 y2 F' L R' D R' D U2 L' U' L R U R' y' U2 L' U' L y L' U' L y R U' R' U R U R' y2 U2";
 const edgeScrambleLeft =
   "F2 U B2 D2 U' F2 D2 B2 R2 D B2 U' L D L2 F2 R D' U B z2 y2 F' L R' D R' D U2 L' U' L R U R' y' U2 L' U' L y L' U' L y R U' R' U R U R' y2 U2 U R U R' U' y L' U' L y2 L' U' L U F U F' y U";
+const edgeScrambleNoYellow =
+  "F2 U B2 D2 U' F2 D2 B2 R2 D B2 U' L D L2 F2 R D' U B z2 y2 F' L R' D R' D U2 L' U' L R U R' y' U2 L' U' L y L' U' L y R U' R' U R U R' y2 U2 U R U R' U' y L' U' L y2 L' U' L U F U F' y U U R U R' U' y L' U' L y2 L' U' L U F U F' y U";
 
 const edgeSteps: EdgeStep[] = [
   {
@@ -46,22 +37,38 @@ const edgeSteps: EdgeStep[] = [
       "Insert the white corner piece back to its spot. (R U R' U')",
     ],
   },
+  {
+    alg: "U R U R' U' y L' U' L U2 y' R U R' U' y L' U' L",
+    description: "No top-layer edges without yellow",
+    detailedExplanation: [
+      "Locate an incorrect edge in the middle layer that doesn’t match its center pieces.",
+      "Hold the cube so the incorrect edge is on the right side of the middle layer.",
+      "Perform the righty algorithm (R U R' U').",
+      "Insert the white corner piece back to its spot. (L' U' L)",
+      "Look for the correct edge in the top layer that matches the colors of the centers.",
+      "Follow the steps of the edge goes to the left or to the right",
+    ],
+  },
 ];
 
 const F2LEdges = () => {
   const rightEdgePlayerRef = useRef<HTMLDivElement>(null);
   const leftEdgePlayerRef = useRef<HTMLDivElement>(null);
+  const noYellowEdgePlayerRef = useRef<HTMLDivElement>(null);
   const rightEdgeTwistyPlayerRef = useRef<TwistyPlayer | null>(null);
   const leftEdgeTwistyPlayerRef = useRef<TwistyPlayer | null>(null);
+  const noYellowEdgeTwistyPlayerRef = useRef<TwistyPlayer | null>(null);
   const isMobile = useMediaQuery("(max-width: 768px)");
   const [isPlayingRightEdge, setIsPlayingRightEdge] = useState(false);
   const [isPlayingLeftEdge, setIsPlayingLeftEdge] = useState(false);
+  const [isPlayingNoYellowEdge, setIsPlayingNoYellowEdge] = useState(false);
   const navigate = useNavigate();
 
   const initializeEdgePlayers = () => {
-    if (rightEdgePlayerRef.current && leftEdgePlayerRef.current) {
+    if (rightEdgePlayerRef.current && leftEdgePlayerRef.current && noYellowEdgePlayerRef.current) {
       rightEdgePlayerRef.current.innerHTML = "";
       leftEdgePlayerRef.current.innerHTML = "";
+      noYellowEdgePlayerRef.current.innerHTML = "";
 
       const rightEdgePlayer = new TwistyPlayer({
         puzzle: "3x3x3",
@@ -88,6 +95,19 @@ const F2LEdges = () => {
       });
       leftEdgeTwistyPlayerRef.current = leftEdgePlayer;
       leftEdgePlayerRef.current.appendChild(leftEdgePlayer);
+
+      const noYellowEdgePlayer = new TwistyPlayer({
+        puzzle: "3x3x3",
+        alg: edgeSteps[2].alg,
+        experimentalSetupAlg: edgeScrambleNoYellow,
+        hintFacelets: "none",
+        backView: "none",
+        background: "none",
+        controlPanel: "none",
+        tempoScale: 0.8,
+      });
+      noYellowEdgeTwistyPlayerRef.current = noYellowEdgePlayer;
+      noYellowEdgePlayerRef.current.appendChild(noYellowEdgePlayer);
     }
   };
 
@@ -117,6 +137,17 @@ const F2LEdges = () => {
     }
   };
 
+  const handlePlayNoYellowEdge = () => {
+    if (noYellowEdgeTwistyPlayerRef.current) {
+      if (isPlayingNoYellowEdge) {
+        noYellowEdgeTwistyPlayerRef.current.pause();
+      } else {
+        noYellowEdgeTwistyPlayerRef.current.play();
+      }
+      setIsPlayingNoYellowEdge(!isPlayingNoYellowEdge);
+    }
+  };
+
   const handleResetEdge = (index: number) => {
     if (index === 0 && rightEdgeTwistyPlayerRef.current) {
       rightEdgeTwistyPlayerRef.current.timestamp = 0;
@@ -124,11 +155,14 @@ const F2LEdges = () => {
     } else if (index === 1 && leftEdgeTwistyPlayerRef.current) {
       leftEdgeTwistyPlayerRef.current.timestamp = 0;
       setIsPlayingLeftEdge(false);
+    } else if (index === 2 && noYellowEdgeTwistyPlayerRef.current) {
+      noYellowEdgeTwistyPlayerRef.current.timestamp = 0;
+      setIsPlayingNoYellowEdge(false);
     }
   };
 
   return (
-    <Container size="xl">
+    <Container size="lg">
       <Title
         order={3}
         mb="xs"
@@ -149,7 +183,8 @@ const F2LEdges = () => {
         direction={isMobile ? "column" : "row"}
         justify="space-between"
         align="stretch"
-        style={{ maxWidth: "980px", margin: "0 auto" }}
+        style={{ maxWidth: "100%", margin: "0 auto" }}
+        wrap="wrap"
       >
         {edgeSteps.map((step, index) => (
           <Flex
@@ -170,7 +205,13 @@ const F2LEdges = () => {
               }}
             >
               <div
-                ref={index === 0 ? rightEdgePlayerRef : leftEdgePlayerRef}
+                ref={
+                  index === 0
+                    ? rightEdgePlayerRef
+                    : index === 1
+                    ? leftEdgePlayerRef
+                    : noYellowEdgePlayerRef
+                }
                 style={{
                   width: "300px",
                   display: "flex",
@@ -182,18 +223,24 @@ const F2LEdges = () => {
                   radius="md"
                   variant="light"
                   onClick={
-                    index === 0 ? handlePlayRightEdge : handlePlayLeftEdge
+                    index === 0
+                      ? handlePlayRightEdge
+                      : index === 1
+                      ? handlePlayLeftEdge
+                      : handlePlayNoYellowEdge
                   }
                   mr="xs"
                   color={
                     (index === 0 && isPlayingRightEdge) ||
-                    (index === 1 && isPlayingLeftEdge)
+                    (index === 1 && isPlayingLeftEdge) ||
+                    (index === 2 && isPlayingNoYellowEdge)
                       ? "red"
                       : "blue"
                   }
                 >
                   {(index === 0 && isPlayingRightEdge) ||
-                  (index === 1 && isPlayingLeftEdge)
+                  (index === 1 && isPlayingLeftEdge) ||
+                  (index === 2 && isPlayingNoYellowEdge)
                     ? "Stop"
                     : "Play"}
                 </Button>
@@ -207,7 +254,7 @@ const F2LEdges = () => {
               </Box>
             </Card>
 
-            <Box>
+            <Box style={{maxWidth: "90%",}}>
               <Text fw={700} size="xl" mb="md">
                 {step.description}
               </Text>
